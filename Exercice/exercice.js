6be90503a401b25c86683302d14fefea1033e874
sadek91220 <sudeni.shinde@gmail.com>
2023-02-28 01:46:01 +0100

    cours js

--- Exercice/exercice.js
+++ Exercice/exercice.js
@@ -49,3 +49,60 @@ let addition = unNombre + unNombre2
 //   let addition = nombre + nombre2
 
 document.write("<br>" + "Le résultat de l'addition est = " + addition)
+
+//-----Exercice 4:
+
+//j'arrive sur un espace securisé au moyen du prenom et de l'age
+//je dois saisir mon prenom et mon age pour m'identifier sur le site (au prealable stockées dans des variable prenom et age)
+// en cas d'echec une alerte m'informe du probleme
+// si tout va bien un message de bienvenue m'accueille
+        
+//        let identifiant = "sadek";
+//        let age3 = 38;
+//
+//        let login = prompt("saisissez votre identifiant")
+//        let verifAge = parseFloat(prompt("Saisissez votre age"))
+//        login = login.toLocaleLowerCase(); // toLocaleLowerCase() pour tout mettre en minuscule et toLocaleUpperCase() pour tout mettre en majuscule
+//        if (identifiant === login && age3 === verifAge) {
+//            alert("Bienvenue " + identifiant + " !" + " Vous etes connecté !")
+//        } else {
+//            alert("Identifiant ou age incorrect")
+//        }
+
+
+//-------------Exercice 6 :
+
+        //I - utilisez la methode slice : tranche pour renvoyer le mot "bananas".
+        let txt = "i can eat bananas all day";
+        txt22 = txt.slice(0,10)
+        txt23 = txt.slice(18,25)
+        console.log(txt22 + txt23)
+
+
+        //II - Alerter le nombre d'éléments dans un tableau, en utilisant la bonne methode de tableau
+        let cars = ["Volvo", "Jeep", "Mercedes"]
+        console.log(cars.length);
+
+        //III - utilisez la splice methode pour supprimer "Orange" et "apple" de fruit
+        let fruits = ["banane", "orange", "Pomme", "kiwi"]
+        fruits.splice(1,2)
+        console.log(fruits);
+
+        //IV - Choisissez l'operateur conditionnel ternaire correct pour alerter "trop jeune" si l'age est inferieur a 18 ans, sinon alerter assez vieux
+        let age4 = 12
+        alert((age4 < 18) ? "trop jeune" : "assez vieux")
+
+        //V - on recoit un nombre, par exemple var number = 42; comment savoir de combien de dizaine (4) et d'unité (2) est composé ce nombre ? avec l'operateur modulo
+        let number = 42
+        let dizaine = number % 10
+        let unite = dizaine % 1
+        console.log(dizaine, unite);
+        
+
+
+        //VI - utilisez la methode string correcte pour remplacer le mot "hello" par le mot "welcome"
+        let txt2 = "hello world";
+        console.log(txt2.replace("hello","welcome"));
+
+        //VII - utilisez les caractere d'echappement pour alerter (We are "Vikings")
+        alert("We are \"Vikings\" !")
